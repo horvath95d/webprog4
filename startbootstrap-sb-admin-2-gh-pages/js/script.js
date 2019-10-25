e02f3e4ae8995f6b9839da8e60f8361f7bd36f7b
horvath95d <43348597+horvath95d@users.noreply.github.com>
2019-10-25 11:34:32 +0200

    fél kész
    Mostmár a project1-ben dolgozzunk, ne a templateben, a kellő cuccokat kiszedtem belőle, grid rendszer működik és pár egyszerű js funkció is meg van

---
+++ startbootstrap-sb-admin-2-gh-pages/js/script.js
@@ -0,0 +1,34 @@
+
+// range
+var range = document.getElementById("range");
+var odds = document.getElementById("odds");
+odds.innerHTML = range.value;
+
+range.oninput = function() {
+    odds.innerHTML = this.value;
+}
+
+// buttons
+var select = document.getElementById("select");
+var options = select.getElementsByTagName("option");
+function first() {
+    select.selectedIndex = 0;
+}
+
+function next() {
+    if (select.selectedIndex == options.length-1)
+        first();
+    else
+        select.selectedIndex = select.selectedIndex+1;
+}
+
+function prev() {
+    if (select.selectedIndex == 0)
+        last();
+    else
+        select.selectedIndex = select.selectedIndex-1;
+}
+
+function last() {
+    select.selectedIndex = options.length-1;
+}

c96fea4bea29f4f2472e63d24c4e85235231b957
ZajaczAkos <55364288+ZajaczAkos@users.noreply.github.com>
2019-10-27 19:43:58 +0100

    beolvasás/darabolás

--- project1/js/script.js
+++ project1/js/script.js
@@ -32,3 +32,32 @@ function prev() {
 function last() {
     select.selectedIndex = options.length-1;
 }
+
+var readFile =  function(filePath) {
+    const fs = require('fs');
+    const readline = require('readline');
+ 
+    //Fájl(ok) beolvasása
+    const fileReader = readline.createInterface({
+        input: fs.createReadStream(filePath),
+        output: process.stdout,
+        console: false
+    });
+
+    //Sorok feldarabolása
+    fileReader.on('line', function (line) {
+        var dataLine = line.split("$$$");
+ 
+        var article = {
+            'title': dataLine[3],
+            'originalLabels': dataLine[2],
+            'recommandedLabels': dataLine[0],
+            'alsoRecommandedLabels': dataLine[1],
+            'text': dataLine[4]
+        };
+        list.push(article);
+        console.log(list.length);
+    });
+    console.log(list.length);
+ 
+}

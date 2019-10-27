3aba2aa4446d2b00f875a2e2ee184ad83328cf9e
Dragonn <55364375+Dragonnhun@users.noreply.github.com>
2019-10-27 21:03:42 +0100

    index.html alja szerkesztve, beolvasás kicsit átírva

--- project1/js/script.js
+++ project1/js/script.js
@@ -34,30 +34,25 @@ function last() {
 }
 
 var readFile =  function(filePath) {
-    const fs = require('fs');
-    const readline = require('readline');
- 
-    //Fájl(ok) beolvasása
-    const fileReader = readline.createInterface({
+    var fs = require('fs');
+    var readline = require('readline'); 
+    var fileReader = readline.createInterface({
         input: fs.createReadStream(filePath),
         output: process.stdout,
         console: false
     });
-
-    //Sorok feldarabolása
     fileReader.on('line', function (line) {
         var dataLine = line.split("$$$");
  
         var article = {
             'title': dataLine[3],
             'originalLabels': dataLine[2],
-            'recommandedLabels': dataLine[0],
-            'alsoRecommandedLabels': dataLine[1],
+            'recommendedLabels': dataLine[0],
+            'alsoRecommendedLabels': dataLine[1],
             'text': dataLine[4]
         };
         list.push(article);
         console.log(list.length);
     });
-    console.log(list.length);
- 
+    console.log(list.length); 
 }

38ba716bf638bc91316e447bb46cbd9c3ee49b08
ZajaczAkos <55364288+ZajaczAkos@users.noreply.github.com>
2019-12-16 19:17:29 +0100

    2.projekt

---
+++ Web4_Beadando/backend/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb+srv://zsombor:semmi@cluster0-pqkfx.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ Web4_Beadando/frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Fő oldal</h1>
+                <p>Üdvözöllek a főoldalunkon!</p>
+                <p>Készítette:</p>
+                <p>-Horváth Dániel</p>
+                <p>-Jakab Ádám</p>
+                <p>-Zajácz Ákos</p>
+            </div>
+        );
+    }
+}
